bd3058d606957e2dca4276b25ba5c7111c0af90f
anti <anti@antis-Laptop.local>
2023-08-20 22:49:29 -0700

    created shop script

--- geninven.py
+++ geninven.py
@@ -8,12 +8,16 @@ items = [
     # Add more items as needed
 ]
 
+shop = input("Enter the name of the shop ")
+
 # Create a context for the app
 with app.app_context():
-    # Get the group_id for the "Material" group
-    group = Group.query.filter_by(name="Shop").first()
+    # Get the group_id for the specified truck name
+    group = Group.query.filter_by(name=shop).first()
+
+    # If the group (truck) doesn't exist, create it
     if group is None:
-        group = Group(name="Shop")
+        group = Group(name=shop)
         db.session.add(group)
         db.session.commit()
 
@@ -21,10 +25,16 @@ with app.app_context():
 
     # Add each item to the database under the specified group (truck)
     for item in items:
-        inventory_item = Inventory(name=item["name"], item_type=item["item_type"],
-                                  group_id=group_id, quantity=item["quantity"])
-        db.session.add(inventory_item)
+        existing_item = Inventory.query.filter_by(name=item["name"], group_id=group_id).first()
+
+        if existing_item:
+            # Update attributes of existing item if needed, e.g., increase quantity
+            existing_item.quantity += item["quantity"]
+        else:
+            # If item doesn't exist for the truck, add a new one
+            inventory_item = Inventory(name=item["name"], item_type=item["item_type"],
+                                      group_id=group_id, quantity=item["quantity"])
+            db.session.add(inventory_item)
 
     # Commit the changes to the database
     db.session.commit()
-
